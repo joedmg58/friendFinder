8be0066b4b38cf4afd817f9701e8286c75bf5d17
Joed Machado <joedmg58@gmail.com>
2018-08-08 18:58:47 -0400

    HTML Routes completed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,11 @@ var path = require( 'path' );
 
 module.exports = function( app ){
 
+    app.get( '/survey', function( req, res ){
+        res.sendFile( path.join( __dirName, '../app/public/survey.html' ) );
+    } );
 
-    
+    app.get( '*', function( req, res ){
+        res.sendFile( path.join( __dirName, '../app/public/home.html' ) );
+    } );
 }
