da8ca266ee59905cff56b72a8883aadfca7619d2
Joed Machado <joedmg58@gmail.com>
2018-08-08 18:18:37 -0400

    friend array created on friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+//List of friends. This teh data for frienFinder app
+
+var friendArray = [
+    {
+        'name': 'Ariel',
+        'photo': '',
+        'scores': [ 5, 4, 5, 4, 3, 2, 1, 2, 2, 5 ]
+    },
+    {
+        'name': 'Ahmed',
+        'photo': '',
+        'scores': [ 4, 5, 4, 3, 4, 1, 2, 3, 3, 4 ]
+    },
+    {
+        'name': 'Helen',
+        'photo': '',
+        'scores': [ 5, 4, 5, 4, 3, 3, 3, 2, 2, 5 ]
+    },
+    {
+        'name': 'Robert',
+        'photo': '',
+        'scores': [ 5, 5, 5, 4, 3, 2, 3, 2, 4, 5 ]
+    },
+    {
+        'name': 'Susan',
+        'photo': '',
+        'scores': [ 4, 4, 5, 4, 3, 4, 3, 2, 2, 5 ]
+    },
+]
+
+module.exports = friendArray;
