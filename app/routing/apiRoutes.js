cb426e20fa86db3e092701c345814e37fa2453dd
Joed Machado <joedmg58@gmail.com>
2018-08-08 21:35:33 -0400

    home finished, working on survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,16 @@
 //API routes
 
+var friendsData = require( '../data/friends' );
+
 module.exports = function( app ){
 
+    app.get( '/api/friends', function( req, res ){
+        res.json( friendsData );
+    } );
 
-    
+    app.post( '/api/friends', function( req, res ){
+        //Friend compatibility logic here
+
+        friendsData.push( req.body );
+    } );
 }
