1c34770304ac6bb1d2c61fc7170f2a84fe6e9ac2
Joed Machado <joedmg58@gmail.com>
2018-08-11 08:56:39 -0400

    finetunning survey ajax querie

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,34 @@ module.exports = function( app ){
     } );
 
     app.post( '/api/friends', function( req, res ){
+
+        /* var body = req.body;
+
+        var friend = {
+            'name': body.name,
+            'photo': body.photo,
+            'scores': [ 
+                parseInt( body.q1Options ), 
+                parseInt( body.q2Options ), 
+                parseInt( body.q3Options ), 
+                parseInt( body.q4Options ), 
+                parseInt( body.q5Options ),
+                parseInt( body.q6Options ),
+                parseInt( body.q7Options ),
+                parseInt( body.q8Options ),
+                parseInt( body.q9Options ),
+                parseInt( body.q10Options ) 
+            ]
+        }
+
         //Friend compatibility logic here
 
-        friendsData.push( req.body );
+        friendsData.push( friend );
+
+        //res.json( friend );
+
+        res.redirect('/'); */
+
+        
     } );
 }
